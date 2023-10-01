9485b9fa4099cfaee32dc2173677a261e268154b
MarianoLlobet5 <137999126+MarianoLlobet5@users.noreply.github.com>
2023-10-01 19:32:55 +0200

    Update Productivizacion_flask.py
    El archivo relativo a las funciones de Flask

--- Productivizacion_flask.py
+++ Productivizacion_flask.py
@@ -1,10 +1,24 @@
-'''He creado esta función básica para que veamos como funciona en local.'''
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from Productivizacion_prueba import analizar_texto
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# Agrega aquí tus funciones de análisis de texto
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/', methods=['GET', 'POST'])
+def mostrar_formulario():
+    if request.method == 'POST':
+        texto_analizar = request.form['texto']  # Obtén el texto del formulario
+        resultado = analizar_texto(texto_analizar)  # Usa tu función para obtener la predicción
+
+        # Redirige al usuario a la página de resultado con la predicción como parámetro
+        return redirect(url_for('mostrar_resultado', resultado=resultado))
+
+    return render_template('formulario.html')
+@app.route('/resultado/<resultado>')
+def mostrar_resultado(resultado):
+    return render_template('resultado.html', resultado=resultado)
+if __name__ == '__main__':
+    app.run(debug=True, port=3500)
+
